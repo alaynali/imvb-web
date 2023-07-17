8f78c743799fe340373d6172039c453533c8b160
Alayna Li <alaynali.123@gmail.com>
2023-07-17 11:45:19 -0700

    create tournament model

--- schedule/models.py
+++ schedule/models.py
@@ -2,3 +2,10 @@ from django.db import models
 
 # Create your models here.
 
+class Tournament(models.Model):
+    name = models.CharField(max_length=200)
+    start_date = models.DateField()
+    end_date = models.DateField(blank=True)
+
+    def __str__(self):
+        return f"{self.name} | {self.start_date.strftime('%m/%d/%y')}-{self.end_date.strftime('%m/%d/%y')}"
