90ffc24745225c1aba2d0184d6401f2ebc2b744c
Alayna Li <alaynali.123@gmail.com>
2023-07-18 16:24:26 -0700

    add annoucements model to home app

--- home/models.py
+++ home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Announcement(models.Model):
+    name = models.CharField(max_length=200)
+    image = models.ImageField()
+    description = models.TextField(max_length=500)
+    hyperlink = models.URLField()
+
+    def __str__(self):
+        return f"{self.name}"
