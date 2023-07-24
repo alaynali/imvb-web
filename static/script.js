920b8dc7eafe3f7720f37fc1c41aee2e9fd0eb3e
Alayna Li <alaynali.123@gmail.com>
2023-07-23 20:23:02 -0700

    add navbar fade in

--- static/script.js
+++ static/script.js
@@ -2,7 +2,7 @@ const header = document.querySelector(".navbar")
 
 window.onscroll = function() {
     var top = window.scrollY;
-    if(top >=100) {
+    if(top) {
         header.classList.add('navbarDark');
     }
     else {
