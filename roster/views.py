a4450eb1b9983894084d58b768c9b7e911416b72
Alayna Li <alaynali.123@gmail.com>
2023-07-24 13:25:22 -0700

    change view to accomodate for 2s and 3s data, and show on main.html

--- roster/views.py
+++ roster/views.py
@@ -9,7 +9,19 @@ def roster_view(request):
     message = 'hello world from roster view!'
     return render(request, 'roster/main.html', {'h': message})
 
+# class RosterList(ListView):
+#     template_name = 'roster/main.html'
+#     queryset = Player.objects.filter(team='Illini 1s').order_by('number')
+#     # paginate_by = 10
+
 class RosterList(ListView):
+    context_object_name = "data"
     template_name = 'roster/main.html'
-    queryset = Player.objects.filter(team='Illini 1s').order_by('number')
-    # paginate_by = 10
+    
+    def get_queryset(self):
+        myset = {
+            "first": Player.objects.filter(team='Illini 1s').order_by('number'),
+            "second": Player.objects.filter(team='Illini 2s').order_by('number'),
+            "third": Player.objects.filter(team='Illini 3s').order_by('number'),
+        }
+        return myset
