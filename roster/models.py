788ea827fe5a75afe346fe5d8c61d2aa67e1083d
Alayna Li <alaynali.123@gmail.com>
2023-07-24 12:50:00 -0700

    formatting for player list

--- roster/models.py
+++ roster/models.py
@@ -7,6 +7,11 @@ TEAM_CHOICES = (
     ('Illini 3s', 'Illini 3s'),
 )
 
+class CustomManager(models.Manager):
+    def delete(self):
+        for obj in self.get_queryset():
+            obj.delete()
+
 class Player(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100, null=True)
@@ -15,7 +20,18 @@ class Player(models.Model):
     position = models.CharField(max_length=50)
     height = models.CharField(help_text="Enter in format [feet]\'[inches]\"", default="", max_length=5)
     major = models.CharField(max_length=50, blank=True)
-    image = models.ImageField(blank=True)
+    image = models.ImageField(upload_to="roster", default="profile_icon.png")
+
+    objects = CustomManager() # assign CustomManager to model
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} | {self.team}"
+    
+    def delete(self, using=None, keep_parents=False):
+        storage = self.image.storage
+
+        if storage.exists(self.image.name):
+            storage.delete(self.image.name)
+
+        super().delete()
+    
