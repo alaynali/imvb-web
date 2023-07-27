86c9d420c148bb714451a6e7ad1250a84b1846b4
Alayna Li <alaynali.123@gmail.com>
2023-07-27 13:22:27 -0700

    stylize carousel, add gradient to background

--- home/models.py
+++ home/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Photo(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=50)
     image = models.ImageField(upload_to='home')
-    description = models.TextField(max_length=200)
+    description = models.TextField(max_length=100)
     hyperlink = models.URLField()
 
     def __str__(self):
@@ -15,8 +15,8 @@ class Announcement(models.Model):
     name = models.CharField(max_length=200)
     image = models.ImageField(upload_to='home', blank=True)
     description = models.TextField(max_length=1000)
-    button_name = models.TextField(max_length=50)
-    button_hyperlink = models.URLField()
+    button_name = models.TextField(max_length=50, blank=True)
+    button_hyperlink = models.URLField(blank=True)
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
