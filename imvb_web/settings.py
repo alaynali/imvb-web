0ad11fffbe27256f5c12cda6783953b071fec593
Alayna Li <alaynali.123@gmail.com>
2023-07-31 12:48:06 -0700

    ummm yeah

--- imvb_web/settings.py
+++ imvb_web/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-0ztx)1&la^zlfnn%o4_tnm7x#fh**p8e!jjp&m+9u49j#@rag0"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'www.mensvolleyball.illinois.edu']
 
