1e342e9797ed7543b28804d0f276bc02eaf5cb4e
Alayna Li <alaynali.123@gmail.com>
2023-08-01 13:17:02 -0700

    try to use static folders in apps

--- imvb_web/settings.py
+++ imvb_web/settings.py
@@ -139,3 +139,4 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
