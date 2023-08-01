5496597698462aa46f778a4a21dc8907d1d57981
Alayna Li <alaynali.123@gmail.com>
2023-08-01 14:53:12 -0700

    try to serve media files

--- imvb_web/settings.py
+++ imvb_web/settings.py
@@ -130,8 +130,8 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static', 
 ]
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = 'http://media.mensvolleyball.web.illinois.edu/'
+MEDIA_ROOT = "/home/mensvolleyball/repositories/imvb-web/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
