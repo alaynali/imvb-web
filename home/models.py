2e841286b38007ed0f327f90581c14b3d0fafc7c
Alayna Li <alaynali.123@gmail.com>
2023-08-08 15:48:21 -0700

    edit hyperlink fields in home

--- home/models.py
+++ home/models.py
@@ -6,7 +6,7 @@ class Photo(models.Model):
     name = models.CharField(max_length=50)
     image = models.ImageField(upload_to='home')
     description = models.TextField(max_length=100)
-    hyperlink = models.URLField()
+    # hyperlink = models.URLField()
 
     def __str__(self):
         return f"{self.name}"
@@ -17,7 +17,7 @@ class Announcement(models.Model):
     description = models.TextField(max_length=1000)
     button_name = models.CharField(max_length=50, blank=True)
     button_hyperlink = models.URLField(blank=True)
-    created = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=True, help_text='Note: this field is REQUIRED if button name is not empty.')
 
     def __str__(self):
         return f"{self.name}"
